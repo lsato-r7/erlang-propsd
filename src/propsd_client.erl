ab7c550c0684f7f17c46fdee8cb364ae0d9c293c
Louis Sato <lsato@rapid7.com>
2017-01-25 13:41:01 -0600

    fixing start_link

--- src/propsd_client.erl
+++ src/propsd_client.erl
@@ -1,13 +1,13 @@
 -module(propsd_client).
 -behaviour(gen_server).
 
--export([start_link/1]).
+-export([start_link/0]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--spec start_link([]) ->
+-spec start_link() ->
   {ok, pid()} | {error, already_started, pid()} | {error, term()}.
-start_link([]) ->
+start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 -spec init([fun()]) -> {ok, {[fun()], map()}}.
